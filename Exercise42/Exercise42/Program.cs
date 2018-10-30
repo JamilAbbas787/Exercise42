ee5accae66edcdc0998d9be09648acd04b2dea01
Jamil Abbas <jamilabbas787@gmail.com>
2018-10-29 21:14:12 -0400

    Complete

---
+++ Exercise42/Exercise42/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string continueAnswer;
+            do
+            {
+                var xCord = new Point();
+                //xCord.Xcoordinate();
+                var yCord = new Point();
+                //yCord.Ycoordinate();
+
+                Console.WriteLine($"You have created a point ({xCord.Xcoordinate()}, {yCord.Ycoordinate()})");
+
+                Console.WriteLine("Would you like to continue (y/n): ");
+                continueAnswer = Console.ReadLine();
+
+            } while (continueAnswer.Equals("y",StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine("Goodbye!");
+        }
+
+    }
+
+    class Point
+    {
+
+        public int Xcoordinate()
+        {
+            bool validateX;
+            int xCoordinate;
+            do
+            {
+                Console.Write("Enter your X coordinate: ");
+                string xCoordinateString = Console.ReadLine();
+                validateX = int.TryParse(xCoordinateString, out xCoordinate);
+
+            } while (!validateX);
+
+            return xCoordinate;
+
+        }
+
+        public int Ycoordinate()
+        {
+            bool validateY;
+            int yCoordinate;
+
+            do
+            {
+                Console.Write("Enter your Y coordinate: ");
+                string yCoordinateString = Console.ReadLine();
+                validateY = int.TryParse(yCoordinateString, out yCoordinate);
+
+            } while (!validateY);
+
+            return yCoordinate;
+        }
+                
+    }
+}
